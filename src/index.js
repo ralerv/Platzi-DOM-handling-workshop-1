4f62b3c6fcdefcaac5add5419706a35b8ad05b33
ralerv <ramos_rrv@hotmail.com>
2023-10-29 16:21:40 -0500

    append data and fetch funcs

--- src/index.js
+++ src/index.js
@@ -4,3 +4,27 @@
  **/
 
 console.log('Happy hacking :)')
+
+const url = "https://platzi-avo.vercel.app/api/avo";
+
+async function fetchData(url){
+    const response = await fetch(url);
+    const data = await response.json();
+    return data
+}
+
+async function appendData(){
+    const avocados = await fetchData(url);
+    let fragment = document.createDocumentFragment();
+    avocados.data.forEach(element => {
+        let image = document.createElement("img")
+        let title = document.createElement("h2")
+        let price = document.createElement("span")
+        const container = document.createElement("div")
+        container.append(image, title, price);
+        fragment.appendChild(container);
+    });
+    document.body.append(fragment);
+}
+
+appendData()
